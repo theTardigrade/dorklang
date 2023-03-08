324503d8cc175ef1ce625fa2a400ec8aef1b5bc1
James Smith <theTardigrade@users.noreply.github.com>
2023-03-08 21:57:10 +0000

    Create sorting methods for memoryCellCollection

--- memory.go
+++ memory.go
@@ -3,3 +3,15 @@ package main
 type memoryCell uint64
 
 type memoryCellCollection []memoryCell
+
+func (collection memoryCellCollection) Len() int {
+	return len(collection)
+}
+
+func (collection memoryCellCollection) Swap(i, j int) {
+	collection[i], collection[j] = collection[j], collection[i]
+}
+
+func (collection memoryCellCollection) Less(i, j int) bool {
+	return collection[i] < collection[j]
+}
